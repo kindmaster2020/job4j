16fdf1e01d90270acb4d0e3fdc6017f0b577c1a3
kindmaster2020 <kindmaster2020@yandex.ru>
2020-02-29 20:37:57 +0300

    Простое число. [#238424]

---
+++ chapter_001/src/main/java/ru/job4j/loop/CheckPrimeNumber.java
@@ -0,0 +1,16 @@
+package ru.job4j.loop;
+
+public class CheckPrimeNumber {
+    public static boolean check(int number) {
+        boolean prime = true;
+        if (number > 1) {
+            for (int var = 2; var < number; var++) {
+                if (number % var == 0) {
+                    prime = false;
+                    break;
+                }
+            }
+        } else prime = false;
+        return prime;
+    }
+}
