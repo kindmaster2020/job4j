de35735801a5836a2fbc0cb825bca448c49d4085
kindmaster2020 <kindmaster2020@yandex.ru>
2020-02-29 18:11:04 +0300

    Протеиновая диета. [#238421]

---
+++ chapter_001/src/main/java/ru/job4j/loop/Fitness.java
@@ -0,0 +1,13 @@
+package ru.job4j.loop;
+
+public class Fitness {
+    public static int calc(int ivan, int nic) {
+        int month = 0;
+        while (ivan <= nic) {
+            ivan *= 3;
+            nic *=2;
+            month++;
+        }
+        return month;
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/loop/FitnessTest.java
@@ -0,0 +1,26 @@
+package ru.job4j.loop;
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+public class FitnessTest {
+    @Test
+    public void whenIvanGreatNic() {
+        int month = Fitness.calc(95,90);
+        assertThat(month, is(0));
+    }
+
+    @Test
+    public void whenIvanLessByOneNic() {
+        int month = Fitness.calc(90,95);
+        assertThat(month, is(1));
+    }
+
+    @Test
+    public void whenIvanLessByFewNic() {
+        int month = Fitness.calc(50,90);
+        assertThat(month, is(2));
+    }
+}
